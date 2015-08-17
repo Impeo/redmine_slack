ddd54e89015c1c0fbfeb616ac80d271265bf27fb
negibouze <itathirdchildnegi@gmail.com>
2015-08-18 00:38:09 +0900

    Add the call hook methods of backlogs
    Necessary to set the custom field.

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -93,7 +93,7 @@ class SlackListener < Redmine::Hook::Listener
 		channel = channel_for_project project
 		url = url_for_project project
 
-		return unless channel and url and Setting.plugin_redmine_slack[:post_wiki_updates] == '1'
+		return unless channel and url and has_additional_hook_for_project(project, "Wiki update")
 
 		user = page.content.author
 		project_url = "<#{object_url project}|#{escape project}>"
@@ -104,6 +104,44 @@ class SlackListener < Redmine::Hook::Listener
 		attachment[:text] = "Update Page : #{page_url} \n comment : #{page.content.comments}"
 
 		speak msg, channel, attachment, url
+	end
+
+	def controller_stories_new_after_save(context={})
+		project = context[:project]
+		if has_additional_hook_for_project(project, "Story create") then
+			params = context[:params]
+			story = context[:story]
+			issue = Issue.find_by_id(story.id)
+			issue.description = ""
+			controller_issues_new_after_save({ :params => params, :issue => issue})
+		end
+	end
+
+	def controller_stories_edit_after_save(context={})
+		project = context[:project]
+		if has_additional_hook_for_project(project, "Story update") then
+			params = context[:params]
+			issue = Issue.find_by_id(params[:id])
+			journal = Journal.find_by_journalized_id(params[:id])
+			controller_issues_edit_after_save({ :params => params, :issue => issue, :journal => journal})
+		end
+	end
+
+	def controller_tasks_new_after_save(context={})
+		project = context[:project]
+		if has_additional_hook_for_project(project, "Task create") then
+			controller_issues_new_after_save(context)
+		end
+	end
+
+	def controller_tasks_edit_after_save(context={})
+		project = context[:project]
+		if has_additional_hook_for_project(project, "Task update") then
+			params = context[:params]
+			issue = Issue.find_by_id(params[:id])
+			journal = Journal.find_by_journalized_id(params[:id])
+			controller_issues_edit_after_save({ :params => params, :issue => issue, :journal => journal})
+		end
 	end
 
 	def speak(msg, channel, attachment=nil, url=nil)
@@ -172,6 +210,18 @@ private
 		else
 			nil
 		end
+	end
+
+	def has_additional_hook_for_project(proj, custom_value)
+		return has_custom_value_for_project(proj, "Additional call hook", custom_value)
+	end
+
+	def has_custom_value_for_project(proj, field_name, custom_value)
+		return false if proj.blank?
+
+		cf = ProjectCustomField.find_by_name(field_name)
+		cv = proj.custom_values.detect { |v| v.custom_field_id == cf.id and v.value == custom_value }
+		return cv != nil
 	end
 
 	def detail_to_field(detail)
